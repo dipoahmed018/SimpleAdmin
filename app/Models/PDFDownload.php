f956ef02d902fd2d5bc29689c0434b4325bbba02
dipoahmed018 <dipoahmed018@gmail.com>
2022-07-24 23:30:59 +0600

    inital setup complete

---
+++ app/Models/PDFDownload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PDFDownload extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pdf_download';
+}
